3cd14f7e6f9a4435b22c720d8fbe12c58bdecec4
Robert Southee <rksouthee@gmail.com>
2024-12-19 08:20:44 +0000

    Solve 2024 day 19 part 2
    Was getting too clever, use caching and just check all towels

--- 2024/day19.cpp
+++ 2024/day19.cpp
@@ -34,69 +34,28 @@ namespace
 		return designs;
 	}
 
-	bool is_possible(const std::string_view design, const std::span<const std::string> towels)
+	S64 count_combinations(const std::string_view design, const std::span<const std::string> towels)
 	{
-		if (design.empty()) return true;
+		static std::unordered_map<std::string_view, S64> cache;
+		if (cache.contains(design))
+		{
+			return cache[design];
+		}
+
+		S64& value = cache[design];
+		if (design.empty())
+		{
+			return value += 1;
+		}
+
 		for (const std::string& towel : towels)
 		{
-			if (design.starts_with(towel) && is_possible(design.substr(towel.size()), towels))
+			if (design.starts_with(towel))
 			{
-				return true;
+				value += count_combinations(design.substr(towel.size()), towels);
 			}
 		}
-		return false;
-	}
-
-	bool is_possible(const std::string_view design, const std::unordered_map<char, std::span<const std::string>>& towels)
-	{
-		if (design.empty()) return true;
-		if (!towels.contains(design[0])) return false;
-		for (const std::string& towel : towels.at(design[0]))
-		{
-			if (design.starts_with(towel) && is_possible(design.substr(towel.size()), towels))
-			{
-				return true;
-			}
-		}
-		return false;
-	}
-
-	std::vector<std::string> unique_towels(std::vector<std::string> towels)
-	{
-		std::vector<std::string> result;
-		for (S64 i = 0; i < std::ssize(towels); ++i)
-		{
-			std::swap(towels[0], towels[i]);
-			if (!is_possible(towels[0], std::span(towels).subspan(1)))
-			{
-				result.push_back(towels[0]);
-			}
-		}
-		std::ranges::sort(result);
-		return result;
-	}
-
-	std::unordered_map<char, std::span<const std::string>> create_map(const std::vector<std::string>& towels)
-	{
-		S64 start = 0;
-		char current = towels[0][0];
-		std::unordered_map<char, std::span<const std::string>> result;
-		for (S64 i = 1; i < std::ssize(towels); ++i)
-		{
-			if (towels[i][0] != current)
-			{
-				result[current] = std::span(towels).subspan(start, i - start);
-				start = i;
-				current = towels[i][0];
-			}
-		}
-		result[current] = std::span(towels).subspan(start);
-		return result;
-	}
-
-	S64 part_one(const std::unordered_map<char, std::span<const std::string>>& towels, const std::vector<std::string>& designs)
-	{
-		return std::ranges::count_if(designs, [&towels](const std::string& design) -> bool { return is_possible(design, towels); });
+		return value;
 	}
 }
 
@@ -104,8 +63,17 @@ SOLVE
 {
 	const std::vector<std::string> towels = read_towels(std::cin);
 	const std::vector<std::string> designs = read_designs(std::cin);
-	const std::vector<std::string> unique = unique_towels(towels);
-	const std::unordered_map<char, std::span<const std::string>> map = create_map(unique);
 
-	std::cout << part_one(map, designs) << '\n';
+	S64 part_one = 0;
+	S64 part_two = 0;
+
+	for (const std::string& design : designs)
+	{
+		const S64 combinations = count_combinations(design, towels);
+		if (combinations) ++part_one;
+		part_two += combinations;
+	}
+
+	std::cout << part_one << '\n';
+	std::cout << part_two << '\n';
 }
