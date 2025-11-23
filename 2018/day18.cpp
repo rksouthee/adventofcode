36869eb10f1c0570e11d7bb1b458cf9c23f6a9d6
Robert Southee <rksouthee@gmail.com>
2025-11-23 15:47:57 +0000

    Solve 2018 day 18
    Add helper to cycle detection algorithms

--- 2018/day18.cpp
+++ 2018/day18.cpp
@@ -1,9 +1,162 @@
 #include "aoc.h"
 
+#include "aoc/orbit_structure.h"
+
 namespace
 {
+using Grid = std::vector<std::string>;
+const char open_ground = '.';
+const char tree = '|';
+const char lumberyard = '#';
+
+Grid read_input(std::istream &is)
+{
+    Grid grid;
+    for (std::string line; std::getline(is, line);)
+    {
+        grid.push_back(std::move(line));
+    }
+    return grid;
 }
+
+std::span<char> get_neighbours(const std::span<char, 8> buffer, const Grid &grid, const S64 x, const S64 y)
+{
+    auto iter = buffer.begin();
+    for (S64 ny = y - 1; ny <= y + 1; ++ny)
+    {
+        if (ny < 0 || ny >= std::ssize(grid))
+        {
+            continue;
+        }
+        const std::string_view line = grid[ny];
+        for (S64 nx = x - 1; nx <= x + 1; ++nx)
+        {
+            if (nx < 0 || nx >= line.size())
+            {
+                continue;
+            }
+            if (nx == x && ny == y)
+            {
+                continue;
+            }
+            *iter++ = line[nx];
+        }
+    }
+    return {buffer.begin(), iter};
+}
+
+char update_item(const Grid &grid, const S64 x, const S64 y)
+{
+    std::array<char, 8> neighbours_buffer;
+    const std::span<char> neighbours = get_neighbours(neighbours_buffer, grid, x, y);
+    const char ch = grid[y][x];
+    switch (ch)
+    {
+    case open_ground: {
+        int tree_count = 0;
+        for (const char nc : neighbours)
+        {
+            if (nc == tree)
+            {
+                ++tree_count;
+                if (tree_count == 3)
+                {
+                    return tree;
+                }
+            }
+        }
+    }
+    break;
+
+    case tree: {
+        int lumberyard_count = 0;
+        for (const char nc : neighbours)
+        {
+            if (nc == lumberyard)
+            {
+                ++lumberyard_count;
+                if (lumberyard_count == 3)
+                {
+                    return lumberyard;
+                }
+            }
+        }
+    }
+    break;
+
+    case lumberyard: {
+        bool has_lumberyard = false;
+        bool has_tree = false;
+        for (const char nc : neighbours)
+        {
+            has_lumberyard |= nc == lumberyard;
+            has_tree |= nc == tree;
+        }
+
+        if (!has_lumberyard || !has_tree)
+        {
+            return open_ground;
+        }
+    }
+    break;
+    }
+
+    return ch;
+}
+
+Grid update(const Grid &grid)
+{
+    Grid new_grid = grid;
+    for (S64 y = 0; y < std::ssize(grid); ++y)
+    {
+        const std::string_view line = grid[y];
+        for (S64 x = 0; x < line.size(); ++x)
+        {
+            new_grid[y][x] = update_item(grid, x, y);
+        }
+    }
+    return new_grid;
+}
+
+S64 calculate_resource_value(const Grid &grid)
+{
+    S64 trees = 0;
+    S64 lumberyards = 0;
+    for (const std::string &line : grid)
+    {
+        for (const char ch : line)
+        {
+            switch (ch)
+            {
+            case tree:
+                ++trees;
+                break;
+            case lumberyard:
+                ++lumberyards;
+                break;
+            }
+        }
+    }
+    return trees * lumberyards;
+}
+
+S64 part_one(const Grid &grid)
+{
+    const S64 iterations = 10;
+    return calculate_resource_value(aoc::power(grid, update, iterations));
+}
+
+S64 part_two(const Grid &grid)
+{
+    const S64 iterations = 1000000000;
+    return calculate_resource_value(aoc::iterate_nonterminating_orbit(grid, update, iterations));
+}
+} // namespace
 
 SOLVE
 {
+    const Grid grid = read_input(std::cin);
+
+    std::cout << part_one(grid) << '\n';
+    std::cout << part_two(grid) << '\n';
 }
--- lib/aoc/orbit_structure.h
+++ lib/aoc/orbit_structure.h
@@ -7,133 +7,134 @@
 
 namespace aoc
 {
-	template <typename F, typename N, typename T>
-	T power(T x, F f, N n)
-	{
-		while (n != N{0})
-		{
-			x = f(x);
-			--n;
-		}
-		return x;
-	}
-
-	template <typename F, typename T>
-	std::size_t distance(T x, const T& y, F f)
-	{
-		std::size_t n = 0;
-		while (x != y)
-		{
-			x = f(x);
-			++n;
-		}
-		return n;
-	}
-
-	template <typename F, typename T>
-	T collision_point_nonterminating_orbit(const T& x, F f)
-	{
-		T slow = x;
-		T fast = f(x);
-
-		while (fast != slow)
-		{
-			slow = f(slow);
-			fast = f(fast);
-			fast = f(fast);
-		}
-		return fast;
-	}
-
-	template <typename F, typename P, typename T>
-	T collision_point(const T& x, F f, P p)
-	{
-		if (!p(x))
-		{
-			return x;
-		}
-
-		T slow = x;
-		T fast = f(x);
-
-		while (fast != slow)
-		{
-			slow = f(slow);
-			if (!p(fast))
-			{
-				return fast;
-			}
-			fast = f(fast);
-			if (!p(fast))
-			{
-				return fast;
-			}
-			fast = f(fast);
-		}
-		return fast;
-	}
-
-	template <typename F, typename T>
-	T convergent_point(T x0, T x1, F f)
-	{
-		while (x0 != x1)
-		{
-			x0 = f(x0);
-			x1 = f(x1);
-		}
-		return x0;
-	}
-
-	template <typename F, typename T>
-	T connection_point_nonterminating_orbit(const T& x, F f)
-	{
-		return aoc::convergent_point(x, f(collision_point_nonterminating_orbit(x, f)), f);
-	}
-
-	template <typename F, typename P, typename T>
-	T connection_point(const T& x, F f, P p)
-	{
-		const T y = aoc::collision_point(x, f, p);
-		if (!p(y))
-		{
-			return y;
-		}
-		return aoc::convergent_point(x, f(y), f);
-	}
-
-	template <typename F, typename P, typename T>
-	bool terminating(const T& x, F f, P p)
-	{
-		return !p(aoc::collision_point(x, f, p));
-	}
-
-	template <typename T>
-	struct Orbit_structure
-	{
-		std::size_t handle_size;
-		std::size_t cycle_size;
-		T connection_point;
-	};
-
-	template <typename F, typename T>
-	Orbit_structure<T> orbit_structure_nonterminating_orbit(const T& x, F f)
-	{
-		T y = aoc::connection_point_nonterminating_orbit(x, f);
-		return { aoc::distance(x, y, f), aoc::distance(f(y), y, f), std::move(y) };
-	}
-
-	template <typename F, typename P, typename T>
-	Orbit_structure<T> orbit_structure(const T& x, F f, P p)
-	{
-		const T y = aoc::connection_point(x, f, p);
-		const std::size_t handle_size = aoc::distance(x, y, f);
-		std::size_t cycle_size = 0;
-		if (p(y))
-		{
-			cycle_size = aoc::distance(f(y), y, f);
-		}
-		return { handle_size, cycle_size, std::move(y) };
-	}
+template <typename F, typename N, typename T> T power(T x, F f, N n)
+{
+    while (n != N{0})
+    {
+        x = f(x);
+        --n;
+    }
+    return x;
 }
+
+template <typename F, typename T> std::size_t distance(T x, const T &y, F f)
+{
+    std::size_t n = 0;
+    while (x != y)
+    {
+        x = f(x);
+        ++n;
+    }
+    return n;
+}
+
+template <typename F, typename T> T collision_point_nonterminating_orbit(const T &x, F f)
+{
+    T slow = x;
+    T fast = f(x);
+
+    while (fast != slow)
+    {
+        slow = f(slow);
+        fast = f(fast);
+        fast = f(fast);
+    }
+    return fast;
+}
+
+template <typename F, typename P, typename T> T collision_point(const T &x, F f, P p)
+{
+    if (!p(x))
+    {
+        return x;
+    }
+
+    T slow = x;
+    T fast = f(x);
+
+    while (fast != slow)
+    {
+        slow = f(slow);
+        if (!p(fast))
+        {
+            return fast;
+        }
+        fast = f(fast);
+        if (!p(fast))
+        {
+            return fast;
+        }
+        fast = f(fast);
+    }
+    return fast;
+}
+
+template <typename F, typename T> T convergent_point(T x0, T x1, F f)
+{
+    while (x0 != x1)
+    {
+        x0 = f(x0);
+        x1 = f(x1);
+    }
+    return x0;
+}
+
+template <typename F, typename T> T connection_point_nonterminating_orbit(const T &x, F f)
+{
+    return aoc::convergent_point(x, f(collision_point_nonterminating_orbit(x, f)), f);
+}
+
+template <typename F, typename P, typename T> T connection_point(const T &x, F f, P p)
+{
+    const T y = aoc::collision_point(x, f, p);
+    if (!p(y))
+    {
+        return y;
+    }
+    return aoc::convergent_point(x, f(y), f);
+}
+
+template <typename F, typename P, typename T> bool terminating(const T &x, F f, P p)
+{
+    return !p(aoc::collision_point(x, f, p));
+}
+
+template <typename T> struct Orbit_structure
+{
+    std::size_t handle_size;
+    std::size_t cycle_size;
+    T connection_point;
+};
+
+template <typename F, typename T> Orbit_structure<T> orbit_structure_nonterminating_orbit(const T &x, F f)
+{
+    T y = aoc::connection_point_nonterminating_orbit(x, f);
+    return {aoc::distance(x, y, f), aoc::distance(f(y), y, f), std::move(y)};
+}
+
+template <typename F, typename P, typename T> Orbit_structure<T> orbit_structure(const T &x, F f, P p)
+{
+    const T y = aoc::connection_point(x, f, p);
+    const std::size_t handle_size = aoc::distance(x, y, f);
+    std::size_t cycle_size = 0;
+    if (p(y))
+    {
+        cycle_size = aoc::distance(f(y), y, f);
+    }
+    return {handle_size, cycle_size, std::move(y)};
+}
+
+template <typename F, typename T> T iterate_nonterminating_orbit(const T &x, const F f, const std::size_t n)
+{
+    const Orbit_structure<T> structure = aoc::orbit_structure_nonterminating_orbit(x, f);
+    if (n < structure.handle_size)
+    {
+        return aoc::power(x, f, n);
+    }
+
+    const std::size_t cycle_offset = (n - structure.handle_size) % (structure.cycle_size + 1);
+    return aoc::power(structure.connection_point, f, cycle_offset);
+}
+} // namespace aoc
 
 #endif
