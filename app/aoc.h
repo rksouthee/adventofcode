08f9d11a41732e4eb5153724a8e33e7df289fdac
Robert Southee <rksouthee@gmail.com>
2025-11-23 15:40:21 +0000

    Add common headers to aoc header
    Added some commonly used headers to aoc.h which is included by all solutions

--- app/aoc.h
+++ app/aoc.h
@@ -1,6 +1,18 @@
 #ifndef AOC_AOC_H
 #define AOC_AOC_H
 
+// Common includes for Advent of Code solutions
+#include "aoc/core.h"
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <span>
+#include <string>
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
 #define SOLVE extern "C" void solve([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 
 #endif
